d98b97712f3849a291e69c239312eab8ff49573a
yulolimum <yulian@infinite.red>
2017-06-20 06:18:57 -0700

    updates the ord list

--- lib/ucb_groups/person.rb
+++ lib/ucb_groups/person.rb
@@ -8,7 +8,7 @@ module UcbGroups
         :last_name => :sn,
         :public_email => :mail,
         :official_email => :ucbemail,
-        :orgs => :berkeleyEduPrimaryDeptUnitHierarchyString
+        :orgs => :berkeleyedudeptunithierarchystring
     }.freeze
 
     attr_accessor *ATTRIBUTE_MAPPINGS.keys
